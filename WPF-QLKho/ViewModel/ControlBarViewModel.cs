60c63607807dcf2a0b95e9832685203c52757edb
Rumi <giacmotuongtuong@gmail.com>
2019-09-20 16:51:31 +0700

    completed ControlBar

---
+++ WPF-QLKho/ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPF_QLKho.ViewModel
+{
+    public class ControlBarViewModel : BaseViewModel
+    {
+        #region command
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand MinimizeWindowCommand { get; set; }
+        public ICommand MaximizeWindowCommand { get; set; }
+        public ICommand MouseLeftButtonDownWindowCommand{ get; set; }
+        
+        #endregion
+        public ControlBarViewModel()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>((p) => { return p == null? false : true; }, (p) => {
+
+                FrameworkElement window = GetWindowParent(p);
+                //ép kiểu w window từ frameworkelement sang kiểu Window để có thể dùng đc command đóng Window 
+                var w = window as Window;
+
+                //Lỡ lấy nhầm thằng đéo phải Window? hay không lấy được! => w phải not null
+                if(w != null)
+                {
+                    w.Close();
+                }
+            });
+
+            MaximizeWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) => {
+
+                FrameworkElement window = GetWindowParent(p);
+                //ép kiểu w window từ frameworkelement sang kiểu Window để có thể dùng đc command đóng Window 
+                var w = window as Window;
+
+                //Lỡ lấy nhầm thằng đéo phải Window? hay không lấy được! => w phải not null
+                if (w != null)
+                {
+                    if (w.WindowState != WindowState.Maximized)
+                    {
+                        w.WindowState = WindowState.Maximized;
+                    }else
+                    {
+                        w.WindowState = WindowState.Normal;
+                    }
+                }
+            });
+
+            MinimizeWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) => {
+
+                FrameworkElement window = GetWindowParent(p);
+                //ép kiểu w window từ frameworkelement sang kiểu Window để có thể dùng đc command đóng Window 
+                var w = window as Window;
+
+                //Lỡ lấy nhầm thằng đéo phải Window? hay không lấy được! => w phải not null
+                if (w != null)
+                {
+                    if (w.WindowState != WindowState.Minimized)
+                    {
+                        w.WindowState = WindowState.Minimized;
+                    }
+                    else
+                    {
+                        w.WindowState = WindowState.Maximized;
+                    }
+                }
+            });
+
+            MouseLeftButtonDownWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) => {
+
+                FrameworkElement window = GetWindowParent(p);
+                //ép kiểu w window từ frameworkelement sang kiểu Window để có thể dùng đc command đóng Window 
+                var w = window as Window;
+
+                //Lỡ lấy nhầm thằng đéo phải Window? hay không lấy được! => w phải not null
+                if (w != null)
+                {
+                    //nắm controlbar drag :)
+                    w.DragMove();
+                }
+            });
+
+        }
+        
+        FrameworkElement GetWindowParent(UserControl p)
+        {
+             FrameworkElement parent =p;
+
+            //when parent == null, parent is window!
+            //if parent not null, parent get Parent from UserControl => return parent when parent = null
+            while(parent.Parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+            return parent;
+        }
+    }
+}
---
+++ WPF-QLKho/ViewModel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPF_QLKho.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        // mọi thứ xử lý sẽ nằm trong này
+        public ICommand LoadedWindowCommand { get; set; }
+        public MainViewModel()
+        {
+            //MessageBox.Show("Đã vào trong MainViewModel -> DataContext của mainwindow.xaml");
+
+            //When MainWindow is loaded > run LoginWindow
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+
+            });
+            
+            
+        }
+    }
+}
